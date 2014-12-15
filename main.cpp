2f5a7dfb1ad796f20f738f4914c53cecc73d97f6
Gabriel Aurelie <agabriel@bs406-s16-13.insa-lyon.fr>
2014-12-15 17:33:49 +0100

    First commit : added class boids and agent and the first methods : perception neighbours and addAgent

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "bwindow.h"
+#include "Agent.h"
+#include "Boids.h"
+
+
+int main()
+{
+
+   
+    Agent a1;
+    printf("%f %f\n ",a1.GetX(),a1.GetY());
+
+    Agent a2;
+    printf("%f %f\n",a2.GetX(),a2.GetY());
+
+    Boids b;
+    b.addAgent(a1);
+    b.addAgent(a2);
+   
+    printf("%d \n",b.perception(a1,a2));
+    int k = b.neighbours(0);
+    printf("%d \n",k);
+
+    /*
+    bwindow win(640,480);
+    printf("%d\n",win.init());
+    win.map();
+    for(;;)
+    {
+	int ev = win.parse_event();
+	switch(ev)
+	{
+	    case BKPRESS :
+		printf("keypressed\n"); 
+		printf("key : %s\n",win.get_lastkey());
+		break;
+	    case BBPRESS:
+		printf("buttonpressed\n"); break;
+	    case BEXPOSE:
+		printf("expose\n"); break;
+	    case BCONFIGURE:
+		printf("configure\n"); break;
+	}
+	win.draw_point(100,100,0xFF00);
+	win.draw_line(100,100,200,200,0xFF0000);
+	win.draw_text(10,10,0x0,"Hello World",strlen("Hello World"));
+
+        
+	win.draw_square(200,200,220,220,0xFF00);
+	win.draw_fsquare(400,400,440,440,0xFF00);
+	
+    }
+    */
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,12 @@
+test.out: main.o bwindow.o Agent.o Boids.o
+	g++ -o test.out main.o bwindow.o Agent.o Boids.o -lX11 -L/usr/X11R6/lib
+main.o: main.cpp 
+	g++ -c main.cpp 
+bwindow.o: bwindow.cpp bwindow.h
+	g++ -c bwindow.cpp
+Agent.o : Agent.cpp Agent.h
+	g++ -c Agent.cpp
+Boids : Boids.cpp Boids.h
+	g++ -c Boids.cpp
+clean :
+	rm *.o *.out
